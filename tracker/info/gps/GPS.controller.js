9b54736a79778578cb902747887ed6345352dc72
ferrygun <ferrygun@yahoo.com>
2014-07-08 22:14:41 +0800

    Update GPS.controller.js

--- tracker/info/gps/GPS.controller.js
+++ tracker/info/gps/GPS.controller.js
@@ -3,7 +3,6 @@ sap.ui.controller("gps.GPS", {
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf ftse_simple.FTSE_SIMPLE
 */
 //	onInit: function() {
 //
@@ -29,7 +28,6 @@ sap.ui.controller("gps.GPS", {
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf ftse_simple.FTSE_SIMPLE
 */
 //	onBeforeRendering: function() {
 //
@@ -38,7 +36,6 @@ sap.ui.controller("gps.GPS", {
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf ftse_simple.FTSE_SIMPLE
 */
 //	onAfterRendering: function() {
 //
@@ -46,7 +43,6 @@ sap.ui.controller("gps.GPS", {
 
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf ftse_simple.FTSE_SIMPLE
 */
 //	onExit: function() {
 //

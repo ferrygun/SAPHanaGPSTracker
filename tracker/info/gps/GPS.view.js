6380962a3488f6517ccc242ac2e7735d8e299f74
ferrygun <ferrygun@yahoo.com>
2014-07-08 22:13:42 +0800

    Update GPS.view.js

--- tracker/info/gps/GPS.view.js
+++ tracker/info/gps/GPS.view.js
@@ -2,7 +2,6 @@ sap.ui.jsview("gps.GPS", {
 
 	/** Specifies the Controller belonging to this View. 
 	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
-	* @memberOf ftse_simple.FTSE_SIMPLE
 	*/ 
 	getControllerName : function() {
 		return "gps.GPS";
@@ -10,7 +9,6 @@ sap.ui.jsview("gps.GPS", {
 
 	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
 	* Since the Controller is given to this method, its event handlers can be attached right away. 
-	* @memberOf ftse_simple.FTSE_SIMPLE
 	*/ 
 	createContent : function(oController) {
 		var oPanel = new sap.ui.commons.Panel().setText('GPS Coordinate');
